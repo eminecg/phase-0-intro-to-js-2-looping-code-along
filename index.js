cab6802161f55cb7927b3915205d8bf112a1d7f3
Emine Çığ <emine.cig67@gmail.com>
2022-05-04 11:16:15 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(namesArray ,event ){
+    let newArr= [];
+
+    for(let i=0;i<namesArray.length;i++ ){
+        newArr.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`); 
+    }
+    return newArr
+}
+
+function countDown ( num ){
+    while( num >= 0 ){
+        console.log(num)
+        num-=1
+
+    }
+    
+}
